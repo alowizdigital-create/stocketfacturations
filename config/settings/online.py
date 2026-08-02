547b75e9e1865be5064b2861fe0e98e699055ab8
alowizdigital-create <alowizdigital@gmail.com>
2026-08-02 19:06:56 +0200

    Update online.py

--- config/settings/online.py
+++ config/settings/online.py
@@ -30,12 +30,28 @@ CSRF_TRUSTED_ORIGINS = env.list(
 # }
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'zweeydb'),
+        'USER': os.getenv('DB_USER', 'zweeyuser'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'Jeanpierre236'),
+        'HOST': os.getenv('DB_HOST', 'stocketfacturation-zweeydatabase-pss441'),
+        'PORT': os.getenv('DB_PORT', '3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
