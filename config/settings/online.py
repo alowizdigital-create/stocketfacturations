b43035dc5a7fb7f3452d65cee5218d6641887b82
alowizdigital-create <alowizdigital@gmail.com>
2026-08-07 15:38:42 +0200

    Update online.py

--- config/settings/online.py
+++ config/settings/online.py
@@ -33,6 +33,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #     }
 # }
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
